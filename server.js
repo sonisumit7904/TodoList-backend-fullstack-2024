4c36baf112c887d9cdd46fe383425d4a454844ea
sonisumit7904 <sonisumit7904@gmail.com>
2024-11-06 08:23:46 +0530

    new first commit

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ connectDB();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("<h1>API is running...</h1>");
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
